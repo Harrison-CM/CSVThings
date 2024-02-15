fc98be4f1047b0ab1ca63c59d26204600691ef25
hclar <hclar@LAPTOP-639L4V9G.wifi.uc.edu>
2024-02-15 08:11:30 -0500

    First commit

--- CSVThings/mainPackage/main.py
+++ CSVThings/mainPackage/main.py
@@ -2,5 +2,21 @@
 
 from utilsPackage.utils import *
 
-# Call the function and store what it returns 
-#  in a variable called data
+if __name__ == "__main__":
+    # Call the function and store what it returns 
+    #  in a variable called data
+    data = read_CSV_file()
+    print("# of rows in data set", len(data))
+    #Print the first and last row
+    print("First Row: ", data[0])
+    print("Last Row: ", data[-1])
+    
+    #Write a list comprehension expression 
+    #To pull out all the collision types into a set
+    
+    collisionTypes = {myData[6] for myData in data}
+    print("# of collision types: ", len(collisionTypes))
+    print(collisionTypes)
+    
+    #For Thursday, Modify ^ expression to prevent the blank type of collision
+
